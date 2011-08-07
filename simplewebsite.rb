d947f8d88443c2200cb45cbc8692cbf9693406b7
Tom Wey <tjmwey@gmail.com>
2011-08-07 22:48:59 +0100

    Underline current nav link

--- simplewebsite.rb
+++ simplewebsite.rb
@@ -4,16 +4,20 @@ require 'haml'
 
 class SimpleWebsite < Sinatra::Base
     set :root, File.dirname(__FILE__)
+    attr_reader :current
 
     get '/' do
+        @current = 'index'
         haml :index
     end
 
     get '/work' do
+        @current = 'work'
         haml :work
     end
 
     get '/links' do
+        @current = 'links'
         haml :links
     end
 end
