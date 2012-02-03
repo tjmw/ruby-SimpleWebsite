c415f485e03fb0fcac597da9cb0eb383a6d2a0a2
Tom Wey <tjmwey@gmail.com>
2012-02-03 23:28:17 +0000

    adding functionality to view response headers from arbitrary URL

--- simplewebsite.rb
+++ simplewebsite.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'sinatra/base'
 require 'haml'
 require 'yaml'
+require 'net/http'
+require 'uri'
 
 class SimpleWebsite < Sinatra::Base
     set :root, File.dirname(__FILE__)
@@ -25,5 +27,11 @@ class SimpleWebsite < Sinatra::Base
     get '/env' do
         filtered_env = request.env.inject({}){|h, (k,v)| h[k] = v if k =~ /^[A-Z_]+$/; h}
         body "<pre>#{filtered_env.to_yaml}</pre>"
+    end
+
+    get '/showmetheheaders' do
+        uri = URI.parse(params[:url])
+        response_headers = Net::HTTP.get_response(uri).to_hash
+        body "<pre>#{response_headers.to_yaml}</pre>"
     end
 end
